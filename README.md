31f271aa5d891664661b19fd191c4df357470392
Himanshu Dias <himanshudias2004@gmail.com>
2024-07-09 19:07:23 +0100

    Added intial spec and req. Coded model part intial

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
 # ProgressManager
+
+Application which can be used to store data about how well the user is doing at uni.
+
+# Requirements
+
+## User Interaction
+
+- The application should allow users to enter the name of course
+- The application should allow users to enter the number of years studying the course and the weight of each year for the degree
+- The application should allow users to enter the names of modules for each year, and the number of credits for each module
+- The application should allow users to add 1/2 sections to each module (exam and coursework) and the weight of each section for the module
+- The application should allow users to add subsections to the coursework section for individual assignments
+- The application should allow users to enter the mark/ percentage recieved for each assignment
+- The application should allow users to enter the mark/ percentage for the exam
+- The user can edit the values of any of these at anytime but under a different mode
+
+## Application Outputs
+
+- There is a main screen which displays the courses undertaken
+- There is a sub screen for each course with displays stats for each year as well as a graph which displays the progress of the course
+- There is a sub screen for each year which displays stats for each module as well as a graph which displays the progress of the year
+- There is a sub screen for each module which displays the stats for coursework and exam as well as a graph which displays the progress of the module
+- There is a sub screen for courseworks which displays stats for each assignment as well as a graph which displays the progress of the courseworks
+
+# Specifications
+
+## User Interaction
+
+### Main (Courses Screen)
+
+- There is an add button on the main screen to add courses
+  - Clicking this will add a widget to the screen with a default name
+  - The widget will have an edit button which lets users change the name of the course
+  - Clicking on the widget will open a new screen for the specific course
+
+### Years Screen
+
+- There is an add button on this screen to add years
+
+- Clicking will add a widget to the screen with an incrementing name
+  - Intially the widget will only have its name as there is no content
+- There is an empty graph on the side which updates as years are added
+- The graph is a line graph, which percentage on the y-axis and year on the x-axis
+- The plot for each year is the weighted grade from that year for the course
+- The line should have alternating colours between the years (black,white,black)
+- There are 4 horizontal lines that cross the y-axis which represent pass, lower second, higher second and first grade lines.
+- There is an edit button on year widget which lets users change the name of the year
+- Clicking on a year widget will open a new screen for that year
+
+### Modules Screen
+
+- There is an add button on this scren to add a module
+
+- Clicking this will open a window where the user has to enter some details of the module (name, credits, exam weight, coursework weight)
+
+        - There will be a button on this window to save these details and add the module widget, or cancel
+
+- The widget will display its name, then the number of credits, and then the current mark for this module.
+
+- There is an empty graph on the side which updates as modules are added
+
+- y-axis same, lines same, x-axis modules which diffeent colours
---
+++ src/model.py
@@ -0,0 +1,210 @@
+class Course:
+    def __init__(self,id,name):
+        self.id = id
+        self.name = name
+        self.grade = 0
+
+
+    def update_grade(self,grade):
+        self.grade = grade
+
+    def update_name(self,nName):
+        self.name = nName
+
+    def get_grade(self):
+        return self.grade
+    
+    def get_name(self):
+        return self.name
+
+
+class Year:
+    def __init__(self,id,name):
+        self.id = id
+        self.name = name
+        self.grade = 0
+
+    def update_grade(self,grade):
+        self.grade = grade
+    
+    def update_name(self,nName):
+        self.name = nName
+
+    def get_grade(self):
+        return self.grade
+    
+    def get_name(self):
+        return self.name
+
+class Module:
+    def __init__(self,id,name,credits):
+        self.id= id
+        self.name = name
+        self.credits = credits
+        self.grade = 0
+
+    def update_name(self,nName):
+        self.name = nName
+
+    def update_credits(self,nCredits):
+        self.credits = nCredits
+
+    def update_grade(self,grade):
+        self.grade= grade
+    
+    def get_name(self):
+        return self.name
+    
+    def get_credits(self):
+        return self.credits
+    
+    def get_grade(self):
+        return self.grade
+
+class Coursework:
+    def __init__(self,weight):
+        self.name = "Coursework"
+        self.weight = weight
+        self.grade = 0
+
+    def update_weight(self,nWeight):
+        self.weight = nWeight
+    
+    def update_grade(self,grade):
+        self.grade = grade
+    
+    def get_weight(self):
+        return self.weight
+    
+    def get_grade(self):
+        return self.grade
+    
+    def get_name(self):
+        return self.name
+
+class Exam:
+    def __init__(self,weight):
+        self.name = "Exam"
+        self.weight = weight
+        self.grade = 0
+
+
+    def update_weight(self,nWeight):
+        self.weight = nWeight
+    
+    def update_grade(self,grade):
+        self.grade = grade
+    
+    def get_weight(self):
+        return self.weight
+    
+    def get_grade(self):
+        return self.grade
+    
+    def get_name(self):
+        return self.name
+    
+
+class Assignment:
+    def __init__(self,id,name,weight):
+        self.id = id
+        self.name = name
+        self.weight = weight
+        self.grade = 0
+
+    def update_weight(self,nWeight):
+        self.weight = nWeight
+    
+    def update_grade(self,grade):
+        self.grade = grade
+    
+    def get_weight(self):
+        return self.weight
+    
+    def get_grade(self):
+        return self.grade
+    
+    def get_name(self):
+        return self.name
+    
+
+
+class courseModel:
+    def __init__(self):
+        self.courses = []
+
+    def add_course(self,course):
+        self.courses.append(course)
+    
+    def rem_course(self,id):
+        del self.courses[id]
+
+    def get_courses(self):
+        return self.courses
+
+  
+
+class yearModel:
+    def __init__(self):
+        self.years = []
+
+    
+    def add_year(self,year):
+        self.years.append(year)
+
+    def rem_year(self,id):
+        del self.years[id]
+
+    def get_years(self):
+        return self.years
+    
+
+class moduleModel:
+    def __init__(self):
+        self.modules = []
+
+    def add_module(self,module):
+        self.modules.append(module)
+    
+    def rem_module(self,id):
+        del self.modules[id]
+    
+    def get_modules(self):
+        return self.modules
+    
+
+class assessmentModel:
+    def __init__(self):
+        self.assessments = []
+    
+    def add_cw(self,cw):
+        self.assessments.append(cw)
+    
+    def add_e(self,e):
+        self.assessments.append(e)
+
+    def get_assessments(self):
+        return self.assessments
+    
+    def rem_assessment(self,id):
+        del self.assessments[id]
+    
+    def get_length(self):
+        return len(self.assessments)
+    
+class assignmentModel:
+    def __init__(self):
+        self.assignments = []
+
+    def add_assignment(self,assignment):
+        self.assignments.append(assignment)
+
+    def rem_assigments(self,id):
+        del self.assignments[id]
+
+    def get_assignments(self):
+        return self.assignments
+    
+
+
+    
