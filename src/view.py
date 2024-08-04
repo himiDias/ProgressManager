4b130914a0a53b9b35624db0fbcf6cac0d7a4886
Himanshu Dias <himanshudias2004@gmail.com>
2024-08-04 23:03:05 +0530

    made skeleton login screen + added main file

--- src/view.py
+++ src/view.py
@@ -8,13 +8,67 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.setWindowTitle("University Progress Tracker")
         self.setGeometry(100,100,1000,800)
+        self.stack = QStackedWidget()
+        self.setCentralWidget(self.stack)
 
-        menu_bar = self.menuBar()
+        self.login_screen = LoginScreen(self)
 
-        view_menu = menu_bar.addMenu('&View')
-        help_menu = menu_bar.addMenu('&Help')
+        self.stack.addWidget(self.login_screen)
+
+        self.stack.setCurrentWidget(self.login_screen)
+
+        self.status_bar = QStatusBar()
+        self.setStatusBar(self.status_bar)
+        self.status_bar.showMessage("ProgressManager v1.0")
 
         self.show()
+
+
+class LoginScreen(QWidget):
+    def __init__(self,main_window):
+        super().__init__()
+        self.main_window = main_window
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+
+        titleL = QLabel('Enter DB credentials')
+        userL = QLabel('[Username Image]')
+        passL = QLabel('[Password Image]')
+        loginB = QPushButton('Login')
+        loginB.clicked.connect(self.checkCred)
+
+        username = QWidget()
+        userLayout = QHBoxLayout()
+        password = QWidget()
+        passwLayout = QHBoxLayout()
+        username.setLayout(userLayout)
+        password.setLayout(passwLayout)
+        self.user = QLineEdit(
+            self,
+            placeholderText = 'Username',
+            maxLength = 20
+        )
+
+        self.passw = QLineEdit(
+            self,
+            placeholderText = 'Password',
+            echoMode = QLineEdit.Password
+        )
+        userLayout.addWidget(userL)
+        userLayout.addWidget(self.user)
+        passwLayout.addWidget(passL)
+        passwLayout.addWidget(self.passw)
+        layout.addWidget(titleL)
+        layout.addWidget(username)
+        layout.addWidget(password)
+        layout.addWidget(loginB)
+
+    
+    def checkCred(self):
+        un = self.user.text()
+        pw = self.passw.text()
+        print(un,pw)
+        pass
 
 
 
